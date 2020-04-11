0e782dd3f50da21b70ccefeba6cb20968a0b60be
HizkiaMumu <hizkiamumu24@gmail.com>
2020-04-11 17:33:14 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,67 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dataMeninggal: any = 0;
+  dataPositif: any = 0;
+  selectedCountry: any;
+  dataSembuh: any = 0;
+  dataCovid: any = [];
+  countries: any;
+
+  constructor(private http: HttpClient) {
+    this.getCountries();
+    this.getGlobalData();
+  }
+
+  getCountries(){
+    this.http.get('https://restcountries.eu/rest/v2/all').subscribe((response) => {
+      this.countries = response;
+      console.log(this.countries);
+    });
+  }
+
+  getGlobalData(){
+    this.http.get('https://covidapi.info/api/v1/global/count').subscribe((response) => {
+      console.log(response);
+      this.dataCovid = [];
+
+      for (let key in response.result) {
+        if (response.result.hasOwnProperty(key)) {
+          console.log(key + " -> " + response.result[key]);
+          response.result[key].date = key;
+          this.dataCovid.push(response.result[key]);
+
+          this.dataPositif = response.result[key].confirmed;
+          this.dataSembuh = response.result[key].recovered;
+          this.dataMeninggal = response.result[key].deaths;
+        }
+      }
+
+      this.dataCovid = this.dataCovid.reverse();
+      console.log(this.dataCovid);
+    });
+  }
+
+  selectCountry(){
+    this.http.get('https://covidapi.info/api/v1/country/' + this.selectedCountry).subscribe((response) => {
+      console.log(response);
+      this.dataCovid = [];
+
+      for (let key in response.result) {
+        if (response.result.hasOwnProperty(key)) {
+          console.log(key + " -> " + response.result[key]);
+          response.result[key].date = key;
+          this.dataCovid.push(response.result[key]);
+
+          this.dataPositif = response.result[key].confirmed;
+          this.dataSembuh = response.result[key].recovered;
+          this.dataMeninggal = response.result[key].deaths;
+        }
+      }
+
+      this.dataCovid = this.dataCovid.reverse();
+      console.log(this.dataCovid);
+    });
+  }
 
 }
